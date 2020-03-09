75788899f7b121235e06e1399c2b448ae8fa8f53
seflmsw <ecsimsw@gamil.com>
2020-03-09 20:10:20 +0900

    master

--- Arithmetic Server/test_a_client.py
+++ Arithmetic Server/test_a_client.py
@@ -1,11 +1,12 @@
 import socket
 
-db_url = "C:/Users/luraw/OneDrive/Desktop/db/test.csv"
+ip = '127.0.0.1'
+port = 7777
+version = 0
 
-def data_read_by_socket():
-    # 접속 정보 설정
-    ip = '127.0.0.1'
-    port = 7777
+db_url = "C:/Users/luraw/OneDrive/Desktop/db/"
+
+def data_receive():
     size = 1024
     sever_addrs = (ip, port)
 
@@ -17,28 +18,30 @@ def data_read_by_socket():
             print(">> Connect ", ip)
 
             c_msg = bytes("client_ready\n", 'UTF-8')
-            ## '\n' 빠트리지 않게, 서버에서 \n을 기다린다.
+            ## '\n' 빠트리지 않게!!, 데이터 받을 때 끝을 표시하는거 잊지말기.
 
-            c.send(c_msg)
+            c.send(c_msg) #client ready
 
-            s_msg = c.recv(1024).decode()
+            s_msg = c.recv(1024).decode() #sever ready
+
+
 
             if s_msg == "server_ready":
                 print("\n>> server_ready")
                 c.send(bytes("client_ACK\n", 'UTF-8'))
 
                 print("\n>> down start")
-                
+
                 with open(db_url, mode="a", encoding='utf8',errors = 'ignore') as f:
                     while True:
                         data = c.recv(1024).decode(errors='ignore')
-                        
+
                         if data.find("!download_end") != -1:
                             data = data.replace("!download_end", "")
                             f.write(data)
                             print(" \n>> download end")
                             break
-                        
+
                         f.write(data)
 
         except Exception as e:
@@ -47,5 +50,3 @@ def data_read_by_socket():
 
 
 result = data_read_by_socket()
-
-#print(a)
